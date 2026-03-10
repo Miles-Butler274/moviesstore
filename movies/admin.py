e5ad1055c242d561a4da02900935855446338374
Mark Devenyi <markdevenyidev@gmail.com>
2026-03-10 19:23:51 -0400

    feat(admin): add top reviewer admin page

--- movies/admin.py
+++ movies/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.template.response import TemplateResponse
+from django.db.models import Count
+from django.urls import path
 from .models import Movie, Review
 
 class MovieAdmin(admin.ModelAdmin):
@@ -6,6 +10,30 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('name', 'price')
 
+class ReviewAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/review_changelist.html'
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                'top-reviewer/',
+                self.admin_site.admin_view(self.top_reviewer_view),
+                name='top-reviewer',
+            ),
+        ]
+        return custom_urls + urls
+
+    def top_reviewer_view(self, request):
+        top_reviewer = User.objects.annotate(
+            total_reviews=Count('review')
+        ).order_by('-total_reviews').first()
+
+        return TemplateResponse(
+            request,
+            'admin/top_reviewer.html',
+            { 'user': top_reviewer, }
+        )
+
+
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Review)
-# Register your models here.
+admin.site.register(Review, ReviewAdmin)
